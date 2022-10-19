158fa3a5f30498157055d62d59b9989ec3fbfc9e
karanbammi <108460399+karanbammi@users.noreply.github.com>
2022-10-20 03:08:44 +0530

    Delete FindYourGYM directory

--- FindYourGYM/backend-findyourgym/connectToDB.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export default function connect(){
-    mongoose.connect('mongodb+srv://admin:admin1234@cluster0.lkt9yay.mongodb.net/?retryWrites=true&w=majority')
-        .then(() => console.log('Connected to MongoDB...'))
-        .catch((error) => console.error('Could not connect to mongoDB', error));  
-}
